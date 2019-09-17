e8382c85a8c9bf544d3bd198e521fdde25b29273
SanghoonJeong <berrycore@gmail.com>
2019-09-17 11:51:44 +0900

    example for data type

--- 01.What_is_python/ex1_hello.py
+++ 01.What_is_python/ex1_hello.py
@@ -1,1 +1,4 @@
-print("hello")
+print("Life is too short, You need Python")
+if 4 in [1,2,3,4,]:
+    print("There is number 4")
+    
---
+++ 01.What_is_python/ex3_data_type.py
@@ -0,0 +1,17 @@
+# Integer
+a = 1
+print(type(1))
+
+# Floating-Point (Real)
+print(type(1.2))
+
+# Complex
+print(type(3j))
+
+# Octal
+print(type(0o25))
+print(0o25.real)
+
+# Hexadecimal
+print(type(0xFF))
+print(0xFF.real)
